698e171bcc22cf6371461d78e72aaf603b6011f6
Rion Halili | Frakton <rion@frakton.com>
2019-11-14 13:07:49 +0100

    Import project

---
+++ src/main/java/com/frakton/FraktonApplication.java
@@ -0,0 +1,13 @@
+package com.frakton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraktonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraktonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frakton/FraktonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frakton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraktonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
